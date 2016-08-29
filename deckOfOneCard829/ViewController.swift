bb5f4bfa138d826486797d4fc2f4c7e904f3c861
Patrick Pahl <ppahl84@yahoo.com>
2016-08-29 09:52:08 -0600

    Initial Commit

---
+++ deckOfOneCard829/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  deckOfOneCard829
+//
+//  Created by Patrick Pahl on 8/29/16.
+//  Copyright © 2016 Patrick Pahl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
